8de66e670e38d1fe8ee3330fcfcab32550c5e554
nishide-dev <nishide.dev@gmail.com>
2023-10-10 09:11:54 +0900

    🐛 raceIdが削除されていた場合は読み込まない

--- toyokouworks-nextjs/src/app/api/raceId/route.ts
+++ toyokouworks-nextjs/src/app/api/raceId/route.ts
@@ -4,6 +4,9 @@ import prisma from '@/lib/prisma'
 
 export const GET = async (request: Request) => {
   const data = await prisma.race.findMany({
+    where: {
+      deleted: false,
+    },
     select: {
       name: true,
       createdAt: true,
